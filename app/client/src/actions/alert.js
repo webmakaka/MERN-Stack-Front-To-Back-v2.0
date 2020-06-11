428dfd3571156fcda02af03f2a46bce6c77b61d7
WebMakaka <marley.nodejs@gmail.com>
2020-06-11 07:47:37 +0300

    fix

--- app/client/src/actions/alert.js
+++ app/client/src/actions/alert.js
@@ -1,4 +1,4 @@
-import { uuid } from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
